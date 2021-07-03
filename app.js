a2b0b0a8a6bf9346024aaa7c1c551012096b7bf9
joy <geniverjoy96@gmail.com>
2021-07-03 14:27:04 +0530

    env test1

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require("express")
-
+require('dotenv').config();
 const port = process.env.PORT || 3000
 
 const app = express()
 
 app.use("*/heartbeat", (req, res) => res.status(200).json({status: 200, message: "I'm fine, Thank you.!"}))
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connected");
+  })
+  .catch((err) => console.log(err));
 
 app.listen(port, () => {
     console.log(`App running on port ${port}`)
